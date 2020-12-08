8c52e3318c212a5c4afd1ed4a694e2bbe378b773
erdemcalik684 <62620690+erdemcalik684@users.noreply.github.com>
2020-12-08 23:27:48 +0300

    UnitOfWork Yapısının Entegresi
    UnitOfWork Yapısının Entegresi yapıldı.

---
+++ WebApplication2/Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWithWebToken.Domain.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        //geriye birşey dönmeyeceğim demektir.
+        Task CompleteAsync();
+    }
+}
---
+++ WebApplication2/Domain/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWithWebToken.Domain.UnitOfWork
+{
+  
+    //NOT UNİTOFWORK KULLANIMI NASIL YAPILIR ? 
+    public class UnitOfWork : IUnitOfWork
+    {
+        //1) vt işlemi yapacağım için context sınıfını elde etmemiz gerekiyor.
+
+        //2) dbcontexte heryerden erişebilmemin sebebi,startup üzerinde services.adddbcontext tanımlamamızdır.
+
+
+        //3) //NOT = AWAIT NE İŞE YARAR.
+        //AWAİT İN BULUNDUĞU YERDEKİ METOTDAN SORGU GELENE KADAR BEKLE,GEÇME.
+        //SORGU GELDİKTEN SONRA GEÇİŞ YAP DEMEKTİR AWAİT...
+        private readonly ApiWithWebTokenDatabaseContext _context;
+        public UnitOfWork(ApiWithWebTokenDatabaseContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
